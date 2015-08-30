104f1f9f9b50c24916291c92d1c24dc1b7267af7
PedroLDN <pedrofrankstein13@hotmail.com>
2015-08-30 12:34:18 -0300

    Conclusão de uma das página de amostra para meu portfólio - Photo View.

---
+++ js/jscript.js
@@ -0,0 +1,46 @@
+﻿(function($){
+	
+	var imgs = [];
+	
+	var img;
+	for(var a=1; a <= 12; a++){
+		img = new Image();
+		img.src = "images/img_" + a + ".jpg";
+		img.alt = "Imagem aplicada à página de forma dinâmica";
+		
+		imgs.push(img);
+	}
+	
+	var container = $('.img-container');
+	var content = "Essa é alguma descrição da imagem, que seria fornecida por um script server-side!";
+	
+	$.each(imgs, function(index, img){
+		
+		var div = container.clone(true, true);
+		div.find(".image").append(img);
+		div.children("h3").text("Nome da Foto");
+		div.children("p").text(content);
+		div.css("display", "block");
+		
+		$('#images-list').append(div);
+		
+	});
+	
+	var lightbox = $("#lightbox");
+	var lb_container = $("lb-container");
+	
+	$(".image img").click(function(){
+		
+		$(this).clone().appendTo("#lb-container");
+		lightbox.css("display", "block");
+		
+	});
+	
+	$("#icon-remove").click(function(){
+		
+		lightbox.css("display", "none");
+		lb_container.empty();
+		
+	})
+	
+}(jQuery));
